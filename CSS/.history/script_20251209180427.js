51c80144680bdd4fbb4224651c1a0675aa945006
robin-webdev <robin.webdev.me@gmail.com>
2025-12-15 20:05:35 +0200

    initial css commit

---
+++ CSS/.history/script_20251209180427.js
@@ -0,0 +1,28 @@
+// Adding Font Families
+// Getting Fonts from websites
+// Linear/Raidal/Conic Gradients
+// Backgroud Images Properties
+// Position - Absolute, Linear, Fixed and Sticky
+// Main and cross Axes
+// Centering a div using translate and absolute properties
+// flex shrink
+// box shadow
+// transform - translate, rotate in different axes
+// inline and block elements
+// clip path
+// display grid
+// shortcuts for adding divs like .child*5
+// nth child selectors in css including cases of n like 2n, 3n etc.
+// background clip text
+// -webkit-text-stroke : width color;
+// -webkit-scrollbar
+// -webkit-scrollbar-thumb
+// MINMAX fUNCTION 
+// grid-gap = row-gap + comumn-gap
+// .child#child$*5
+// Grid Areas
+// Units in css - pixels, percent, vw(viewport's width), vmax (viewport maximum), vmin (viewport minimum), em, rem
+// SCSS - Nesting, Mixin
+
+
+// THE GUY :  h.20.12b
