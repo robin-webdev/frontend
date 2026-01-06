10e877d784ef1cf79a866c1b321edcbae4ced7c8
robin-webdev <robin.webdev.me@gmail.com>
2026-01-06 23:34:48 +0200

    made a piano with event listener

---
+++ JavaScript/Piano/piano.js
@@ -0,0 +1,43 @@
+const body = document.body;
+
+const pianoKeys = Array.from({ length: 26 }, (_, i) => ({
+  key: String.fromCharCode(65 + i),
+  sound: `${28 + i}.mp3`,
+}));
+
+pianoKeys.forEach((act) => {
+  const aud = new Audio(`./assets/${act.sound}`);
+  //   aud.play();
+});
+
+function getRandom(m) {
+  return Math.floor(Math.random() * m);
+}
+
+body.addEventListener("keydown", (evt) => {
+  let { key } = evt;
+  const act = pianoKeys.find((a) => a.key === key.toUpperCase());
+
+  if (!act) return;
+
+  const aud = new Audio(`./assets/${act.sound}`);
+  aud.play();
+
+  const keys = document.querySelectorAll(".justkey");
+  const rand = getRandom(keys.length);
+  keys.forEach((key2, i) => {
+    if (i === rand) {
+      let ele = keys[i];
+      console.log(ele);
+      ele.style.backgroundColor = key2.classList.contains("keyB")
+        ? "gray"
+        : "gray";
+
+      setTimeout(() => {
+        ele.style.backgroundColor = key2.classList.contains("keyB")
+          ? "black"
+          : "white";
+      }, 100);
+    }
+  });
+});
