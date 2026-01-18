5216e6ec562854eb928631ce71983a8138174699
robin-webdev <robin.webdev.me@gmail.com>
2026-01-18 17:14:17 +0200

    React JS Axios

---
+++ Reactjs/.history/Reactjs_01/src/App_20260118171309.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Marks from "./assets/components/Marks";
+
+const App = () => {
+  return (
+    <div>
+      {/* <Marks /> */}
+      A
+    </div>
+  );
+};
+
+export default App;
---
+++ Reactjs/.history/Reactjs_01/src/App_20260118171311.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Marks from "./assets/components/Marks";
+
+const App = () => {
+  return (
+    <div>
+      {/* <Marks /> */}
+      
+    </div>
+  );
+};
+
+export default App;
