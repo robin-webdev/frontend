163236215fca8e4a3a0fa2f5915d9f60aee1d863
robin-webdev <robin.webdev.me@gmail.com>
2026-01-19 20:23:48 +0200

    ggs

--- Reactjs/Reactjs_01/src/App.jsx
+++ Reactjs/Reactjs_01/src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import Marks from "./assets/components/Marks";
-import Axios from "./assets/components/Axios";
+import Marks from "./components/Marks";
+import Axios from "./components/Axios";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="h-screen w-screen flex items-center justify-center">
+      <Routes>
+        <Route path="/" element={<Marks />} />
+      </Routes>
       {/* <Marks /> */}
       <Axios />
     </div>
